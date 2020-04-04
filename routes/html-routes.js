e468558fee62c9441941b47fa44254c3ee5996ea
Brian Monteverde <bmonteve@gmail.com>
2020-04-04 11:55:10 -0700

    Added initial server files with authentication

---
+++ routes/html-routes.js
@@ -0,0 +1,10 @@
+//Requires path to create relative routes to our HTML files
+var path = require("path");
+
+//Uses middleware to check if user is logged in
+var isAuthenticated = require ("../config/middleware/isAuthenticated");
+
+module.exports = function (app){
+
+    
+}
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 const travisTest = "Test code to see if Travis CI works";
+
+//Requires NPM packages
+var express = require("express");
+var session = require("express-session");
+
+//Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+//Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 3333;
+var db = require("./models");
+
+//Creating express app and configuring middleware needed for authentication 
+var app = express ();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Sessions to create tracking of user's login status
+app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+//Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function (){
+        console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    })
+})
+
+
