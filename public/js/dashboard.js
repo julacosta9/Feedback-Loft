4cfe2003ffa9453c7841cd3ef9c976b346209d8e
Brian Monteverde <bmonteve@gmail.com>
2020-04-09 16:22:47 -0700

    Added beginnings to get project requests for the User

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,14 @@
-console.log("sup yall");
+$(document).ready(function () {
+    $.get("/api/user_data", function (req, res) {
+        var UserId = req.id;
+        getProject(UserId);
+    });
+
+    function getProject(UserId) {
+        $.get("/api/getProjects/" + UserId, function (req, res) {})
+            .then(function () {})
+            .catch(function (err) {
+                console.log(err);
+            })
+    };
+});
