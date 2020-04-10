1759b7d21a2664d111f98d33d35a90cd299e06a3
Brian Monteverde <bmonteve@gmail.com>
2020-04-10 16:22:32 -0700

    fixed requirement for embed link from soundcloud

--- models/project.js
+++ models/project.js
@@ -13,7 +13,7 @@ module.exports = function (sequelize, DataTypes) {
             defaultValue: "music",
         },
         url: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
         description: {
