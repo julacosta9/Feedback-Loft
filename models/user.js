2df84bdb3ff009eeff05891d068133cfe63fe4fb
Brian Monteverde <bmonteve@gmail.com>
2020-04-11 16:29:00 -0700

    Fixed changes

--- models/user.js
+++ models/user.js
@@ -17,15 +17,17 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    feedback_given: {
+    feedback_received: {
         type: DataTypes.INTEGER,
         allowNull: true
     },
-    feedback_received: {
+    feedback_given: {
         type: DataTypes.INTEGER,
         allowNull: true
     }
   });
+
+
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
