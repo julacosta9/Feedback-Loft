34d935f61da94d90ba718a119ee34923470505b5
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-04-11 14:00:40 -0700

    adjust ajax call on feedButton. may be able to delete later

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,18 +6,31 @@ $(document).ready(function () {
 
     function getProject(UserId) {
         $.get("/api/getProjects/" + UserId, function (req, res) {})
-            .then(function () {
-                
-            })
+            .then(function () {})
             .catch(function (err) {
                 console.log(err);
-            })
-    };
+            });
+    }
 
-    $("#feedButton").on("click", function (){
-        $.get("/giveFeedback", function (req, res) {})
-        .catch(function (err) {
-            console.log(err);
+    // $("#feedButton").on("click", function () {
+    //     $.get("/api/getProjectAlgo/", function (req, res) {})
+    //         .then(function (data) {
+    //             console.log(data);
+    //             res.render("project-review", {projectData: data[0].dataValues});
+    //         })
+    //         .catch(function (err) {
+    //             console.log(err);
+    //         });
+    // });
+
+    $("#feedButton").on("click", function () {
+        $.ajax({
+            crossOrigin: true,
+            type: "GET",
+            crossDomain: false,
+            url: "/api/getProjectAlgo",
+            success: function (data) {
+            },
         });
-    })
+    });
 });
