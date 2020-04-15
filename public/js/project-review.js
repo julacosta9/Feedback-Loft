cec5a6007e0a65c5a962c0d2c6e9414f2549e130
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-04-14 17:59:38 -0700

    working feedback submit form and routes

--- public/js/project-review.js
+++ public/js/project-review.js
@@ -9,3 +9,49 @@ $( document ).ready(function() {
     }
   });
 });
+
+$("#feedbackSubmit").on("click", function(event) {
+
+  $.ajax({
+    crossOrigin: true,
+    type: "PUT",
+    data: {
+      feedback_given: parseInt($("#feedback_given").text()) + 1
+    },
+    crossDomain: false,
+    url: "/api/feedbackGivenUpdate",
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
+  $.ajax({
+    crossOrigin: true,
+    type: "POST",
+    data: {
+      text: $("#feedback_text").val().trim(),
+      rating: $('input[name="rating"]:checked').val(),
+      ProjectId: parseInt($("#project-name").attr("data-id"))
+    },
+    crossDomain: false,
+    url: "/api/newFeedback",
+    success: function(data) {
+      console.log(data);
+      console.log(data.createdAt);
+
+      $.ajax({
+        crossOrigin: true,
+        type: "PUT",
+        data: {
+          last_commented: data.createdAt.split("T")[0],
+          projectId: parseInt($("#project-name").attr("data-id"))
+        },
+        crossDomain: false,
+        url: "/api/updateLastCommented",
+        success: function(data) {
+          console.log(data);
+        }
+      });
+    }
+  });
+})
